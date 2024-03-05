d15000f428695c8ed41be6550bc305c24634cb4d
Luka Matic <maticluka888@gmail.com>
2024-03-05 17:03:12 +0100

    init game and display table

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,13 +9,39 @@
 # -----------
 #  1 | 2 | 3
 
+def init_game():
+    '''Initializes the table and prints the instruction.'''
+    print('The game can begin!\n')
+    
+    print('You play by picking numbers which represent fields of the table.\n')
+    print(f' 7 | 8 | 9 ')
+    print('-----------')
+    print(f' 4 | 5 | 6 ')
+    print('-----------')
+    print(f' 1 | 2 | 3 ')
+    print()
 
-print('Welcome to Tic-Tac-Toe game!')
+    return {'1': ' ', '2': ' ', '3': ' ', '4': ' ', '5': ' ', '6': ' ', '7': ' ', '8': ' ', '9': ' '}
+
+
+def display_table(table):
+    '''Prints the current status of the table.'''
+    print('Current status of the table:\n')
+    print(f' {table["7"]} | {table["8"]} | {table["9"]} ')
+    print('-----------')
+    print(f' {table["4"]} | {table["5"]} | {table["6"]} ')
+    print('-----------')
+    print(f' {table["1"]} | {table["2"]} | {table["3"]} ')
+
+
+print('Welcome to Tic-Tac-Toe game!\n')
 
 game_on = True
 
 while game_on:
     
+    # Player 1 gets to choose X or O, Player 2 gets assigned the remaining one
+
     player1 = ''
     player2 = ''
     while player1 not in ('x', 'X', 'o', 'O'):
@@ -28,7 +54,17 @@ while game_on:
     else:
         player2 = 'X'
         
-    print(f'OK! Player 2 is {player2} then!')
+    print(f'OK! Player 2 is {player2} then!\n')
+    
+    ############################################
+    
+    # start of the game
+    
+    table = init_game()
+        
+    # display the table
+    
+    display_table(table)
     
     game_on = False
         
